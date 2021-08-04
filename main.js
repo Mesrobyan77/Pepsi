b18037f897a9cc2f7b561cdbe9b54a7615c1e9f3
Mesrobyan77 <xachik.mesrobyan@inbox.ru>
2021-08-04 18:33:15 +0400

    end

---
+++ main.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function($){
+
+	$("#pepsi001").click(function(){
+		 $("#pepsilogo").attr("src","images/pepsi001.png")
+		 $("#wrapper").css("backgroundColor","#0063be")
+	})
+
+	$("#pepsi002").click(function(){
+	 	 $("#pepsilogo").attr("src","images/pepsi002.png")
+	 	 $("#wrapper").css("backgroundColor","#e60c2c")
+	})
+
+	$("#pepsi003").click(function(){
+		  $("#pepsilogo").attr("src","images/pepsi003.png")
+		  $("#wrapper").css("backgroundColor","#1e1e1e")
+	})
+
+})
